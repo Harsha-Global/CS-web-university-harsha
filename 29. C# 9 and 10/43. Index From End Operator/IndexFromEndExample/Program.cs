018a6d2c07911cdc475a53c73e22e24ceb5bf6f5
Harsha-Global <53711684+Harsha-Global@users.noreply.github.com>
2023-07-19 22:39:13 +0530

    C#

---
+++ 29. C# 9 and 10/43. Index From End Operator/IndexFromEndExample/Program.cs
@@ -0,0 +1,16 @@
+﻿//create an array
+List<string> myFriends = new List<string>() { "Scott", "Allen", "Jones", "James", "Sara" };
+
+//traditional way
+Console.WriteLine(myFriends[myFriends.Count - 1]);
+
+//"index from end" operator (^)
+Console.WriteLine(myFriends[^1]); //Sara
+Console.WriteLine(myFriends[^5]); //Scott
+
+//Index struct
+Index i = ^4;
+Console.WriteLine(myFriends[i]); //Scott
+
+Console.ReadKey();
+
