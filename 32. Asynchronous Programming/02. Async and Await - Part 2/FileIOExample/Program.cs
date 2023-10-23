5b0485fa9cff68174c5c9e6ccade94ffd1d8b40b
Harsha-Global <53711684+Harsha-Global@users.noreply.github.com>
2023-10-23 17:20:33 +0530

    C#

---
+++ 32. Asynchronous Programming/02. Async and Await - Part 2/FileIOExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+class FileWriter
+{
+  public async Task WriteFile(string fileName, string data)
+  {
+    StreamWriter writer = new StreamWriter(fileName);
+    Task writerTask = writer.WriteAsync(data);
+    await writerTask;
+    writer.Close();
+  }
+}
+
+class FileReader
+{
+  public async Task<string> ReadFile(string fileName)
+  {
+    StreamReader reader = new StreamReader(fileName);
+    Task<string> readerTask = reader.ReadToEndAsync();
+    string content = await readerTask;
+    
+    reader.Close();
+    return content;
+  }
+}
+
+class Program
+{
+  async static Task Main()
+  {
+    //Initialize
+    string fileName = @"c:\CSharp\India.txt";
+    FileWriter fileWriter = new FileWriter();
+    FileReader fileReader = new FileReader();
+
+    //Write data to a file asynchronously
+    Task writerTask = fileWriter.WriteFile(fileName, "India is the most populous country by 2023");
+    await writerTask; //Block until the write operation is completed
+    Console.WriteLine("File written.");
+
+    //Read data from the file asynchronously
+    Task<string> readerTask = fileReader.ReadFile(fileName);
+    await readerTask; //Block the current thread until the read operation is completed.
+    Console.WriteLine("File read.");
+
+    Console.WriteLine($"\nFile content: {readerTask.Result}");
+
+    Console.ReadKey();
+  }
+}
+
---
+++ 32. Asynchronous Programming/05. Debugging async methods/FileIOExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+class FileWriter
+{
+  public async Task WriteFile(string fileName, string data)
+  {
+    using (StreamWriter writer = new StreamWriter(fileName))
+    {
+      await writer.WriteAsync(data);
+    }
+  }
+}
+
+class FileReader
+{
+  public async Task<string> ReadFile(string fileName)
+  {
+    throw new NotSupportedException();
+
+    using (StreamReader reader = new StreamReader(fileName))
+    {
+      string content = await reader.ReadToEndAsync();
+      return content;
+    }
+  }
+}
+
+class Program
+{
+  async static Task Main()
+  {
+    //Initialize
+    string fileName = @"c:\CSharp\India.txt";
+    FileWriter fileWriter = new FileWriter();
+    FileReader fileReader = new FileReader();
+
+    //Write data to a file asynchronously
+    await fileWriter.WriteFile(fileName, "India is the most populous country by 2023"); //Block the current task (but without blocking the current thread
+    Console.WriteLine("File written.");
+
+    try
+    {
+      //Read data from the file asynchronously
+      string content = await fileReader.ReadFile(fileName); //Block the current task (but without blocking the current thread
+      Console.WriteLine("File read.");
+
+      Console.WriteLine($"\nFile content: {content}");
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine(ex.Message);
+    }
+
+    Console.ReadKey();
+  }
+}
+
